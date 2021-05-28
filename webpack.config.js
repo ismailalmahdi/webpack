aa575d1c72bad10a9067d594eadf9c74e1bfed78
admin <meow@meow.local>
2021-05-29 01:19:50 +0800

    webpacker html plugin test

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: './src/index.js',
     output: {
-        
         filename: "main.js",
         path: path.resolve(__dirname,"dist")
     },
-    // optimization:{
-    //     splitChunks: {
-    //         chunks: 'all'
-    //     }
-    // },
-    plugins : [ new HtmlWebpackPlugin()],
+    plugins : [ new HtmlWebpackPlugin() ],
     devServer: {
         contentBase: path.join(__dirname,"dist"),
         port: 9000
